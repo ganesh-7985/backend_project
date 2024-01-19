a14854260c24d9ea70c15224b67cd05292de9ffd
ganesh-7985 <shankarganesh3005@gmail.com>
2024-01-19 22:19:06 +0530

    added user model

---
+++ src/models/user.model.js
@@ -0,0 +1,50 @@
+import mongoose from "mongoose";
+
+const userSchema = mongoose.Schema({
+    username:{
+        type:String,
+        required:true,
+        unique:true,
+        lowercase:true,
+        trim:true,
+        index:true
+    },
+    password:{
+        type:String,
+        required:[true,'password is required'],
+    },
+    email:{
+        type:String,
+        required:true,
+        unique:true,
+        lowercase:true,
+        trim:true
+    },
+    fullname:{
+        type:String,
+        required:true,
+        unique:true,
+        lowercase:true,
+        trim:true
+    },
+    avatar:{
+        type:String,
+        required:true
+    },
+    coverImage:{
+        type:String,   
+    },
+    watchHistory:{
+        type:object.Schema.type.id,
+        ref:'Videos'
+    },
+    refreshToken:{
+        type:String
+    }
+},{timestamps:true})
+
+const User = mongoose.model("User",userSchema);
+
+module.exports={
+  User
+}
