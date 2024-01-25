88c3c6520094e08a22a28d479244ee55171da7f8
ganesh-7985 <shankarganesh3005@gmail.com>
2024-01-25 14:12:06 +0530

    added video model

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,6 +1,8 @@
 import mongoose from "mongoose";
+import Jwt  from "jsonwebtoken";
+import bcrypt from 'bcrypt'
 
-const userSchema = mongoose.Schema({
+const userSchema = new mongoose.Schema({
     username:{
         type:String,
         required:true,
@@ -42,6 +44,7 @@ const userSchema = mongoose.Schema({
         type:String
     }
 },{timestamps:true})
+
 
 const User = mongoose.model("User",userSchema);
 
